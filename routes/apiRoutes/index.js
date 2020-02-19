f4ff6657c55dd5b8ed36bdd5ef378c57a2f9461c
Rachel Iaconianni <iaconiannir@gmail.com>
2020-02-19 15:23:29 -0500

    push working, css looks better

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,24 +1,9 @@
 const router = require('express').Router();
-const {notes} = require('../../data/db.json')
-
-// function findById(id, notesArray) {
-//   const result = notesArray.filter(notes => notes.id === id)[0];
-//   return result;
-// }
+const {notes} = require('../../data/db.json');
 
 router.get('/api/notes', (req, res) => {
   return res.json(notes);
 });
-
-// router.get('/api/notes/:id', (req, res) => {
-//   const result = findById(req.params.id, notes);
-//   console.log(result);
-//   if (result) {
-//     res.json(result);
-//   } else{
-//     res.send(404);
-//   }
-// });
 
 router.post('/api/notes', (req, res) => {
   console.log(req.body);
